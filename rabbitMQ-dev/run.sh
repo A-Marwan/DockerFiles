0dcab73058e13b3d9c803e71e4ceeb22e67b9684
Jacques Moati <jacques@moati.net>
2015-03-30 12:05:24 +0200

    FIX vhost permission

--- rabbitMQ-dev/run.sh
+++ rabbitMQ-dev/run.sh
@@ -3,6 +3,7 @@ chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
 
 rabbitmqctl add_user admin admin
 rabbitmqctl set_user_tags admin administrator
+rabbitmqctl set_permissions admin ".*" ".*" ".*"
 
 /etc/init.d/rabbitmq-server stop
 
