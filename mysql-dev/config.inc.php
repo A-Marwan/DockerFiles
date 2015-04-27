c8595f4c5351778d1cae3651641a91993db04009
Jacques Moati <jacques@moati.net>
2015-04-27 16:05:19 +0200

    Add phpmyadmin to mysql-dev

---
+++ mysql-dev/config.inc.php
@@ -0,0 +1,34 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * phpMyAdmin sample configuration, you can use it as base for
+ * manual configuration. For easier setup you can use setup/
+ *
+ * All directives are explained in documentation in the doc/ folder
+ * or at <http://docs.phpmyadmin.net/>.
+ *
+ * @package PhpMyAdmin
+ */
+
+/*
+ * This is needed for cookie based authentication to encrypt password in
+ * cookie
+ */
+$cfg['blowfish_secret'] = 'a8b7c6d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+
+/*
+ * Servers configuration
+ */
+/* Servers configuration */
+$i = 0;
+
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['port'] = '';
+$cfg['Servers'][$i]['socket'] = '';
+$cfg['Servers'][$i]['ssl'] = false;
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
---
+++ mysql-dev/run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+VOLUME_HOME="/var/lib/mysql"
+CONF_FILE="/etc/mysql/conf.d/my.cnf"
+LOG="/var/log/mysql/error.log"
+
+# Set permission of config file
+chmod 644 ${CONF_FILE}
+chmod 644 /etc/mysql/conf.d/mysqld_charset.cnf
+
+
+StartMySQL ()
+{
+    /usr/bin/mysqld_safe > /dev/null 2>&1 &
+
+    # Time out in 1 minute
+    LOOP_LIMIT=13
+    for (( i=0 ; ; i++ )); do
+        if [ ${i} -eq ${LOOP_LIMIT} ]; then
+            echo "Time out. Error log is shown as below:"
+            tail -n 100 ${LOG}
+            exit 1
+        fi
+        echo "=> Waiting for confirmation of MySQL service startup, trying ${i}/${LOOP_LIMIT} ..."
+        sleep 5
+        mysql -uroot -e "status" > /dev/null 2>&1 && break
+    done
+}
+
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+    if [ ! -f /usr/share/mysql/my-default.cnf ] ; then
+        cp /etc/mysql/my.cnf /usr/share/mysql/my-default.cnf
+    fi
+    mysql_install_db > /dev/null 2>&1
+    
+    StartMySQL
+    mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'
+    mysqladmin -uroot shutdown
+    
+    echo "=> Done!"
+else
+    echo "=> Using an existing volume of MySQL"
+fi
+
+tail -F $LOG &
+
+source /etc/apache2/envvars
+apache2
+exec mysqld_safe
